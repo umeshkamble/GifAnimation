146f44569ebd4a9c67ce231320d103f55e71bb12
Umesh <ukamble6@gmail.com>
2023-02-25 16:57:18 +0400

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace GifAnimation;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    protected async override void OnAppearing()
+    {
+        base.OnAppearing();
+        await Task.Delay(500);
+        imgLoader.IsAnimationPlaying = false;
+        imgLoader.IsAnimationPlaying = true;
+    }
+    private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
