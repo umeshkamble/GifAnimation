1a069b008f32254e0cbf7c8a80a99deacbb29d10
Umesh <ukamble6@gmail.com>
2023-05-03 15:31:31 +0400

    Update MauiProgram.cs

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Controls.Compatibility.Hosting;
 
 namespace GifAnimation;
 
@@ -9,11 +10,18 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+			.UseMauiCompatibility()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+			})
+			 .ConfigureMauiHandlers(handlers =>
+             		{
+#if IOS
+                 handlers.AddCompatibilityRenderer(typeof(GIFImage), typeof(Microsoft.Maui.Controls.Compatibility.Platform.iOS.ImageRenderer));
+#endif
+             		});
 
 #if DEBUG
 		builder.Logging.AddDebug();
